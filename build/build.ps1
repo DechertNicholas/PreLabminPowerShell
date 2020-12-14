1fcab2ad01c844dacb461bab6dd1fdb71c3dfccf
Nicholas Dechert <dechert.nicholas@gmail.com>
2020-12-13 17:50:34 -0800

    Correct variable names for new pipeline job

--- build/build.ps1
+++ build/build.ps1
@@ -44,7 +44,7 @@ if ($null -eq $env:AGENT_ID) {
 
 ## Update build version in manifest
 Write-Output "Updating module version"
-Update-ModuleManifest -Path $env:manifestPath -ModuleVersion $(GitVersion.SemVer)
+Update-ModuleManifest -Path $env:manifestPath -ModuleVersion $env:GITVERSION_SEMVER
 
 ## Find all of the public functions
 Write-Output "Gathering function names for manifest"
